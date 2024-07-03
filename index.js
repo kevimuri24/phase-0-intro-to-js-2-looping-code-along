c3c19ccb4da8c78febb760928cae826bde33fdb5
Kevin Muriuki Karimi <kevimuri24@gmail.com>
2024-07-03 21:59:25 +0300

    Finished lab

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
-// Code your solutions in this file
+
+function writeCards(cards, event){
+    const newArray = [];
+    for (let i = 0; i < cards.length; i++) {
+        newArray.push(`Thank you, ${cards[i]}, for the wonderful ${event} gift!`);
+    }
+    return newArray
+}
+console.log(writeCards(["Guadalupe", "Ollie", "Aki"], "surprise"));
+
+const countDown = number => {
+    while (number >= 0){
+        console.log(number)
+        number--
+    }
+}
+countDown(5)
